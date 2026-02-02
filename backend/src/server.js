a61628d14281ac0728d09456f940e1bf337b0f5d
emmanueldairo <emmanueldairo@cmail.carleton.ca>
2026-02-02 16:53:56 -0500

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,19 +5,33 @@ import cors from "cors";
 import noteRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "../config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
+import path from "path";
 
 dotenv.config();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 //const express = require('express');
 
 const app = express();
 
-
-app.use(cors("http://localhost:5173"));
+if (process.env.NODE_ENV !== 'production') {
+    app.use(cors(
+        {"origin": "http://localhost:5173", 
+    }));
+}
 app.use(express.json());
 app.use(rateLimiter);
 
 app.use('/api/notes', noteRoutes);
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend','dist', 'index.html'));
+    });
+}
+
+
 
 connectDB().then(() => {
    app.listen(PORT, () => {
